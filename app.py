9244dea1bd18961f1f820447720993b4ef2e9a45
olela28 <130731715+olela28@users.noreply.github.com>
2024-10-02 10:46:14 +0000

    87

--- app.py
+++ app.py
@@ -5,6 +5,4 @@ app = Flask(__name__)
 
 @app.route("/projects", methods=["GET"])
 def projects():
-
-    if request.method == "GET":
-        return render_template("/projects.html")
+        return render_template("projects.html")
